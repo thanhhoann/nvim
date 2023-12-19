ba171c5ec82d16488731eea37475a453f4542251
thanhhoann <pdth456@gmail.com>
2023-12-20 00:30:07 +0700

    remove legendary.nvim

--- lua/configs/lazy.lua
+++ lua/configs/lazy.lua
@@ -60,15 +60,6 @@ local UTILS = {
         -- Optional dependencies
         dependencies = { "nvim-tree/nvim-web-devicons" },
     },
-    {
-        'mrjones2014/legendary.nvim',
-        -- since legendary.nvim handles all your keymaps/commands,
-        -- its recommended to load legendary.nvim before other plugins
-        priority = 10000,
-        lazy = false,
-        -- sqlite is only needed if you want to use frecency sorting
-        -- dependencies = { 'kkharji/sqlite.lua' }
-    }
 }
 
 local EDITING_SUPPORT = {
